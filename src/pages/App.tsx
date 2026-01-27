7749fe2a80e47d15e1378fce237dc5eddf35facd
Niklas Dahlqvist <dalkmania@gmail.com>
2026-01-26 19:10:06 -0800

    Small corrections

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -11,7 +11,7 @@ export const App = () => {
       <div className="prose lg:prose-xl max-w-none! py-12">
         <h1 className="w-full text-center">Who's in Space ?</h1>
         <p className="lead text-center">
-          A Website about Space, the International Space Station (ISS) and who
+          A website about Space, the International Space Station (ISS) and who
           is currently up there. The station's mission is to serve as a unique
           microgravity laboratory for scientific research, enabling long-term
           human space exploration and providing benefits to Earth.
