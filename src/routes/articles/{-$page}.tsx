b3f7ecf6ca4b6ff7c8d4f04b99d13bd8f4fe3f8d
Niklas Dahlqvist <dalkmania@gmail.com>
2026-02-21 09:19:09 -0800

    Fix Offset

--- src/routes/articles/{-$page}.tsx
+++ src/routes/articles/{-$page}.tsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute('/articles/{-$page}')({
   loader: async ({ params }) => {
     const perPage = 12
     const page = Number(params.page) || 1
-    const offset = page > 1 ? perPage * page : 0
+    const offset = page > 1 ? perPage * page - perPage : 0
     const response = await fetch(
       `https://api.spaceflightnewsapi.net/v4/articles/?limit=${perPage}${offset > 0 ? `&offset=${offset}` : ``}&format=json&news_site=NASA`,
     )
